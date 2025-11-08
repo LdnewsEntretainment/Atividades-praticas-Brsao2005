16a62c015eb3215d69f5c115d0580e23e0413809
Leandro Duarte <152192578+LdnewsEntretainment@users.noreply.github.com>
2025-11-08 18:44:04 -0300

    Create 2_calculadora_soma.py

---
+++ 2_calculadora_soma.py
@@ -0,0 +1,11 @@
+numero1 = 12
+numero2 = 14
+
+# Cálculo da soma
+soma = numero1 + numero2
+
+# Exibição do resultado
+print("--- Calculadora de Soma ---")
+print(f"O primeiro número é: {numero1}")
+print(f"O segundo número é: {numero2}")
+print(f"Resultado da Soma: {soma}")
